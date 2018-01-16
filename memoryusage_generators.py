db561d1fbdb76ca10c798c37f849d490fe05e3d3
seethakris <seethakris@gmail.com>
2018-01-17 09:45:13 +1100

    Fixed issues with time

--- memoryusage_generators.py
+++ memoryusage_generators.py
@@ -12,20 +12,17 @@ precision = 4
 def some_math(n=100000):
     """ Defines a List expression that appends to list and calculates mean and standard deviation"""
 
-    starttime = time.time()
     numbers = [i * i for i in range(0, n)]
     total = sum(numbers)
-    mean = total / len(numbers)
+    #mean = total / len(numbers)
     mean = np.mean(numbers)
     std = np.std(numbers)
     print(f' Mean, std is {mean:0.3f} , {std:0.3f}')
-    print(f' Execution time {time.time() - starttime : 0.3f} seconds')
 
 
 @profile(precision=precision)
-def some_math_with_generator(n=10000):
+def some_math_with_generator(n=100000):
     """ Defines a Generator function to calculate mean and standard deviation"""
-    starttime = time.time()
     numbers = (i * i for i in range(0, n))
 
     # Find mean
@@ -38,14 +35,12 @@ def some_math_with_generator(n=10000):
     numbers = (i * i for i in range(0, n))
     deviation = 0
     for count, num in enumerate(numbers, 1):
-        deviation = deviation + np.power(num - mean, 2)
+        deviation += (num - mean)** 2
     std = np.sqrt(deviation / count)
 
     print(f' Mean, std is {mean:0.3f} , {std:0.3f}')
-    print(f' Execution time {time.time() - starttime : 0.3f} seconds')
 
 
 if __name__ == '__main__':
     some_math()
-    time.sleep(5)
     some_math_with_generator()
